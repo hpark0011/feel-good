56f27bad253cb1d8f7669bd5e19a9875743c83d8
Hyunsol Park <hpark0011@gmail.com>
2025-08-27 18:57:38 +0900

    add actions

--- app/insights/page.tsx
+++ app/insights/page.tsx
@@ -12,48 +12,61 @@ export default function InsightsPage() {
       <TrendGroupWrapper>
         <div className='text-text-muted text-sm w-full flex items-center gap-1'>
           <Icon name='SparkleIcon' className='size-5 text-icon-light -ml-1' />{" "}
-          Top actions
+          Top actions you can take
         </div>
         <div className='space-y-3'>
           <div className='text-text-strong text-lg w-full'>
             Take these actions to get more opportunities.
           </div>
-          <div className='grid grid-cols-5 gap-2 w-[calc(100%+16px)] -ml-2'>
-            <InsightsCardWrapper orientation='vertical' className='items-start'>
-              <InsightsCardLabel>Chats Exchanged</InsightsCardLabel>
-              <InsightsCardValueWrapper>
-                <InsightsCardValue>232</InsightsCardValue>
-                <GrowthRate isPositive>10%</GrowthRate>
-              </InsightsCardValueWrapper>
-            </InsightsCardWrapper>
-            <InsightsCardWrapper orientation='vertical' className='items-start'>
-              <InsightsCardLabel>Chats Exchanged</InsightsCardLabel>
-              <InsightsCardValueWrapper>
-                <InsightsCardValue>232</InsightsCardValue>
-                <GrowthRate isPositive>10%</GrowthRate>
-              </InsightsCardValueWrapper>
-            </InsightsCardWrapper>
-            <InsightsCardWrapper orientation='vertical' className='items-start'>
-              <InsightsCardLabel>Chats Exchanged</InsightsCardLabel>
-              <InsightsCardValueWrapper>
-                <InsightsCardValue>232</InsightsCardValue>
-                <GrowthRate isPositive>10%</GrowthRate>
-              </InsightsCardValueWrapper>
-            </InsightsCardWrapper>
-            <InsightsCardWrapper orientation='vertical' className='items-start'>
-              <InsightsCardLabel>Chats Exchanged</InsightsCardLabel>
-              <InsightsCardValueWrapper>
-                <InsightsCardValue>232</InsightsCardValue>
-                <GrowthRate isPositive>10%</GrowthRate>
-              </InsightsCardValueWrapper>
-            </InsightsCardWrapper>
-            <InsightsCardWrapper orientation='vertical' className='items-start'>
-              <InsightsCardLabel>Chats Exchanged</InsightsCardLabel>
-              <InsightsCardValueWrapper>
-                <InsightsCardValue>232</InsightsCardValue>
-                <GrowthRate isPositive>10%</GrowthRate>
-              </InsightsCardValueWrapper>
-            </InsightsCardWrapper>
+          <div className='grid grid-cols-3 gap-2 w-[calc(100%+16px)] -ml-2'>
+            <ActionCardWrapper className='gap-2 justify-start'>
+              <div className='flex flex-col gap-1'>
+                <div className='text-text-tertiary text-sm leading-[120%] items-baseline'>
+                  Create content on
+                  <div className='inline-flex text-[13px] mx-1 py-0.5 px-2 bg-orange-500/7 text-orange-500 rounded-md'>
+                    Healthy Diet
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-col gap-0'>
+                <div className='text-text-tertiary text-sm'>
+                  • 24 users asked about this
+                </div>
+                <div className='text-text-tertiary text-sm'>
+                  • Get more qualified leads
+                </div>
+              </div>
+            </ActionCardWrapper>
+            <ActionCardWrapper className='gap-2 justify-start'>
+              <div className='text-text-tertiary text-sm leading-[120%] items-baseline'>
+                Reach out to
+                <div className='inline-flex text-[13px] mx-1 py-0.5 px-2 bg-orange-500/7 text-orange-500 rounded-md'>
+                  @chefjohnny-1443
+                </div>
+              </div>
+              <div className='flex flex-col gap-0'>
+                <div className='text-text-tertiary text-sm'>• 78% match</div>
+                <div className='text-text-tertiary text-sm'>
+                  • Potential client
+                </div>
+              </div>
+            </ActionCardWrapper>
+            <ActionCardWrapper className='gap-2'>
+              <div className='flex flex-col gap-1'>
+                <div className='text-text-tertiary text-sm leading-[120%]'>
+                  Add
+                  <div className='inline-flex text-[13px] mx-1 py-0.5 px-2 bg-orange-500/7 text-orange-500 rounded-md'>
+                    Use simpler language
+                  </div>
+                  to your clone's speaking style.
+                </div>
+              </div>
+              <div className='flex flex-col gap-0'>
+                <div className='text-text-tertiary text-sm leading-[120%]'>
+                  • 3 users had hard time reading your clone's response
+                </div>
+              </div>
+            </ActionCardWrapper>
           </div>
         </div>
       </TrendGroupWrapper>
@@ -211,6 +224,25 @@ export function TrendItem({ children }: { children: React.ReactNode }) {
   );
 }
 
+export function ActionCardWrapper({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "rounded-xl w-full border p-3 py-2.5 flex flex-col justify-between items-start relative  transition-all duration-200 translate-y-0  scale-100 ease-out group bg-white/30 border-white/30  gap-0 inset-shadow-none shadow-xs hover:bg-base hover:shadow-[0_12px_12px_-6px_rgba(255,255,255,0.9),_0_14px_14px_-6px_rgba(0,0,0,0.3) hover:translate-y-[-1px] hover:border-opacity-100 hover:scale-[1.02] cursor-pointer gap-2 justify-start",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
 export function InsightsCardWrapper({
   children,
   isHoverable = true,
@@ -225,7 +257,7 @@ export function InsightsCardWrapper({
   return (
     <div
       className={cn(
-        "rounded-xl w-full border p-4 py-3 flex flex-row justify-between items-center relative  transition-all duration-200 translate-y-0  scale-100 ease-out group bg-white/30 border-white/30  gap-0 inset-shadow-none shadow-xs",
+        "rounded-xl w-full border p-3 py-2.5 flex flex-row justify-between items-center relative  transition-all duration-200 translate-y-0  scale-100 ease-out group bg-white/30 border-white/30  gap-0 inset-shadow-none shadow-xs",
         orientation === "vertical" && "flex-col items-start",
         className,
         isHoverable &&
