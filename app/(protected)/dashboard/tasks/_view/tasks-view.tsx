83a39ea13be4d58347461897dc70b3813697a37a
Hyunsol Park <hpark0011@gmail.com>
2026-01-19 12:06:30 +0900

    small code formatting

--- app/(protected)/dashboard/tasks/_view/tasks-view.tsx
+++ app/(protected)/dashboard/tasks/_view/tasks-view.tsx
@@ -2,7 +2,7 @@
 
 import { LayoutModeProvider } from "@/features/kanban-board";
 import { TasksHeader } from "../_components/tasks-header";
-import { TasksBody } from "./tasks-body";
+import { TasksBody } from "../_components/tasks-body";
 
 export function TasksView() {
   return (
