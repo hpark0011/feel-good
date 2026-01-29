46c383541fcd4016a11b7c54811f275f61600c8f
Hyunsol Park <hpark0011@gmail.com>
2026-01-29 10:16:14 +0900

    create theme provider in pakcage

---
+++ apps/mirror/app/ui-factory/_providers/root-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export function RootProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
