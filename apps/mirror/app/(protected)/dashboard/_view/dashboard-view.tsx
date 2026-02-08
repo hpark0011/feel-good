20682280eee9b3ab9f2588fd6f54699ad8cc1ea7
Hyunsol Park <hpark0011@gmail.com>
2026-02-08 13:50:50 +0900

    add comments in dashboard

--- apps/mirror/app/(protected)/dashboard/_view/dashboard-view.tsx
+++ apps/mirror/app/(protected)/dashboard/_view/dashboard-view.tsx
@@ -3,14 +3,17 @@ import { ProfileImage } from "@/app/(protected)/dashboard/_components/profile-im
 export function DashboardView() {
   return (
     <div className="w-full flex flex-col items-center ">
+      {/* Profile Name */}
       <div className="text-3xl font-medium text-center mt-[100px]">
         Rick Rubin
       </div>
 
+      {/* Profile Image */}
       <div className="flex flex-col gap-2 items-center pt-[100px]">
         <ProfileImage />
       </div>
 
+      {/* Profile Bio */}
       <div className="text-lg text-center max-w-md mx-auto mt-[120px] leading-[1.3]">
         Rick Rubin has been a singular, transformative creative muse for artists
         across genres and generations — from the Beastie Boys to Johnny Cash,
