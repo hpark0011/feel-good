d1f00e13f23c5ed1d8f0bb219af9bea0393d6bf8
Hyunsol Park <hpark0011@gmail.com>
2026-02-11 23:28:01 +0900

    add buttons in article toolbar

--- apps/mirror/features/articles/components/article-toolbar.tsx
+++ apps/mirror/features/articles/components/article-toolbar.tsx
@@ -19,29 +19,58 @@ export function ArticleToolbar(
   const hasSelection = selectedCount > 0;
 
   return (
-    <div className="flex items-center gap-2.5 h-8 px-px">
-      {hasSelection && (
-        <AlertDialog>
-          <DeleteArticlesDialog count={selectedCount} onConfirm={onDelete} />
-          <AlertDialogTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon-sm"
-              disabled={!hasSelection}
-              aria-label={hasSelection
-                ? `Delete ${selectedCount} selected`
-                : "Delete"}
-            >
-              <Icon name="TrashFillIcon" />
-            </Button>
-          </AlertDialogTrigger>
-        </AlertDialog>
-      )}
-      {hasSelection && (
-        <span className="text-sm text-muted-foreground">
-          {selectedCount} selected
-        </span>
-      )}
+    <div className="flex items-center gap-2 px-px justify-end my-3 pr-[2px]">
+      <div className="flex items-center gap-0">
+        <div className="flex items-center gap-2">
+          {hasSelection && (
+            <span className="text-sm text-muted-foreground">
+              {selectedCount} selected
+            </span>
+          )}
+
+          <AlertDialog>
+            <DeleteArticlesDialog count={selectedCount} onConfirm={onDelete} />
+            <AlertDialogTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon-sm"
+                disabled={!hasSelection}
+                aria-label={hasSelection
+                  ? `Delete ${selectedCount} selected`
+                  : "Delete"}
+              >
+                <Icon name="TrashFillIcon" />
+              </Button>
+            </AlertDialogTrigger>
+          </AlertDialog>
+        </div>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+        >
+          <Icon name="MagnifyingGlassIcon" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+        >
+          <Icon name="ArrowUpAndDownIcon" className="size-4.5" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+        >
+          <Icon name="Line3Icon" />
+        </Button>
+      </div>
+      <Button
+        variant="primary"
+        size="sm"
+        className="has-[>svg]:gap-0.5 has-[>svg]:pl-1.5"
+      >
+        <Icon name="PlusIcon" className="text-icon-light size-4.5" />
+        New
+      </Button>
     </div>
   );
 }
