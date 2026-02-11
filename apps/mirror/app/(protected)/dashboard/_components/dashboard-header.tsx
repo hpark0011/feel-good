b7680c47608095ef22f5025e5ce46a72ea4d8a38
Hyunsol Park <hpark0011@gmail.com>
2026-02-11 13:38:35 +0900

    create tasks from code review

--- apps/mirror/app/(protected)/dashboard/_components/dashboard-header.tsx
+++ apps/mirror/app/(protected)/dashboard/_components/dashboard-header.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeToggleButton } from "@feel-good/features/theme/components";
 import { Icon } from "@feel-good/ui/components/icon";
+import { Button } from "@feel-good/ui/primitives/button";
 import { cn } from "@feel-good/utils/cn";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
